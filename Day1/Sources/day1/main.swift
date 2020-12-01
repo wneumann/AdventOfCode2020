d521426af0cdbc8e15d7a941f37639f5cb013357
William D. Neumann <wneumann@gmail.com>
2020-12-01 11:48:50 -0600

    redone using ParsableArgumens instead of ParseableCommand, I think it's a bit easier to read

--- Day1/Sources/day1/main.swift
+++ Day1/Sources/day1/main.swift
@@ -1,50 +1,50 @@
 import ArgumentParser
 import Foundation
 
-struct PairFinder: ParsableCommand {
+struct RunOptions: ParsableArguments {
   @Argument(help: "The location of the input file.", transform: URL.init(fileURLWithPath:))
   var inURL: URL
-  
-  private func findPair(expenses: ArraySlice<Int>, target: Int) -> (Int, Int)? {
-    var pairSet = Set<Int>()
-    for expense in expenses {
-      let match = target - expense
-      if pairSet.contains(expense) {
-        return (expense, match)
-      } else {
-        pairSet.insert(match)
-      }
-    }
-    return nil
-  }
 
-  mutating func run() throws {
-    guard
-      let input = try? String(contentsOf: inURL, encoding: .utf8)
-        .trimmingCharacters(in: .whitespacesAndNewlines)
-    else { fatalError("Invalid file path") }
-    var expenses: ArraySlice<Int> = input.components(separatedBy: .newlines).map {
-      guard let i = Int($0) else { fatalError("Invald input: Could not convert \($0) to Int") }
-      return i
-    }.sorted()[...]
-    print("Looking for pairs:")
-    if let (i,j) = findPair(expenses: expenses, target: 2020) {
-      print("\tFound pair: \(i) * \(j) = \(i * (j))")
+  @Flag var skipPair = false
+  @Flag var skipTriple = false
+}
+
+func findPair(expenses: ArraySlice<Int>, target: Int) -> (Int, Int)? {
+  var pairSet = Set<Int>()
+  for expense in expenses {
+    let match = target - expense
+    if pairSet.contains(expense) {
+      return (expense, match)
     } else {
-      fatalError("No pairs found")
+      pairSet.insert(match)
     }
+  }
+  return nil
+}
 
-    print("Looking for triplets:")
-    let maxMin = 2020 / 3
-    while let i = expenses.popFirst(), i < maxMin {
-      let target = 2020 - i
-      if let (j, k) = findPair(expenses: expenses, target: target) {
-        print("\tTriplets found: \(i) * \(j) * \(k) = \(i * j * k)")
-        return
-      }
-    }
-    print("No triplets found")
+let options = RunOptions.parseOrExit()
+
+guard
+  let input = try? String(contentsOf: options.inURL, encoding: .utf8)
+else { fatalError("Invalid file path") }
+
+var expenses: ArraySlice<Int> = input.components(separatedBy: .newlines).compactMap(Int.init)
+  .sorted()[...]
+
+if !options.skipPair {
+  print("Looking for pairs:")
+  if let (i, j) = findPair(expenses: expenses, target: 2020) {
+    print("\tFound pair: \(i) * \(j) = \(i * (j))")
   }
 }
 
-PairFinder.main()
+if !options.skipTriple {
+  print("Looking for triplets:")
+  let maxMin = 2020 / 3
+  while let i = expenses.popFirst(), i < maxMin {
+    let target = 2020 - i
+    if let (j, k) = findPair(expenses: expenses, target: target) {
+      print("\tTriplets found: \(i) * \(j) * \(k) = \(i * j * k)")
+    }
+  }
+}
