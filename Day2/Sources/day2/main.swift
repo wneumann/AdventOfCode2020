27736c06bcaa19832ed0c8074e12fb7535e30c79
William D. Neumann <wneumann@gmail.com>
2020-12-02 09:07:31 -0600

    Solution for both parts, will assuredly tweak until I like it.

--- Day2/Sources/day2/main.swift
+++ Day2/Sources/day2/main.swift
@@ -13,25 +13,44 @@ struct RunOptions: ParsableArguments {
 let options = RunOptions.parseOrExit()
 
 // MARK: - Actual work done here
+@available(OSX 10.15, *)
 extension String {
-  var isValidPassword: Bool {
+  func passwordComponents() -> (Int, Int, Character, String)? {
     let charactersToBeSkipped = CharacterSet(charactersIn: " -:")
     let scanner = Scanner(string: self)
-    guard let lowerBound = scanner.scanInt() else { return false }
+    guard let lowerBound = scanner.scanInt() else { return nil }
     _ = scanner.scanCharacters(from: charactersToBeSkipped)
-    guard let upperBound = scanner.scanInt(), upperBound >= lowerBound else { return false }
+    guard let upperBound = scanner.scanInt(), upperBound >= lowerBound else { return nil }
     _ = scanner.scanCharacters(from: charactersToBeSkipped)
-    guard let requiredCharacter = scanner.scanCharacter() else { return false }
+    guard let requiredCharacter = scanner.scanCharacter() else { return nil }
     _ = scanner.scanCharacters(from: charactersToBeSkipped)
-    guard let password = scanner.scanUpToCharacters(from: charactersToBeSkipped) else { return false }
+    guard let password = scanner.scanUpToCharacters(from: charactersToBeSkipped) else { return nil }
+    return (lowerBound, upperBound, requiredCharacter, password)
+  }
+  
+  var isValidPassword: Bool {
+    guard let (lowerBound, upperBound, requiredCharacter, password) = self.passwordComponents() else { return false }
     return lowerBound...upperBound ~= password.filter { $0 == requiredCharacter }.count
+  }
+  
+  var isReallyValidPassword: Bool {
+    guard let (firstOffset, secondOffset, comparisonCharacter, password) = self.passwordComponents() else { return false }
+    guard let firstCharacter = password.dropFirst(firstOffset - 1).first else { return false }
+    guard let secondCharacter = password.dropFirst(secondOffset - 1).first else { return false }
+    return (firstCharacter == comparisonCharacter) != (secondCharacter == comparisonCharacter)
   }
 }
 
 let passwords =
   try String(contentsOf: options.inURL, encoding: .utf8)
-        .components(separatedBy: .newlines).compactMap(Int.init)[...]
+        .components(separatedBy: .newlines)
 
 
-let valids = passwords.filter(\.isValidPassword).count
-print("There are \() valid passwords in the database")
+if #available(OSX 10.15, *) {
+  let valids = passwords.filter(\.isValidPassword).count
+  print("There are \(valids) valid passwords in the database")
+  let reallyValids = passwords.filter(\.isReallyValidPassword).count
+  print("There are \(reallyValids) valid passwords in the database")
+} else {
+  print("Update your damn OS already!")
+}
