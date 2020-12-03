97e8142d6e54371844f9bb8d60d185f8d1661cd2
William D. Neumann <wneumann@gmail.com>
2020-12-02 18:19:36 -0600

    Inital commit for day 3, bones of the app

---
+++ Day3/Sources/day3/main.swift
@@ -0,0 +1,15 @@
+import ArgumentParser
+import Foundation
+
+// MARK: - Command line parsing
+struct RunOptions: ParsableArguments {
+  @Argument(help: "The location of the input file.", transform: URL.init(fileURLWithPath:))
+  var inURL: URL
+
+  @Flag var skipStar1 = false
+  @Flag var skipStar2 = false
+}
+
+let options = RunOptions.parseOrExit()
+
+// MARK: - Actual work done here
