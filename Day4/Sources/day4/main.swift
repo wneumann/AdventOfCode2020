b3197bf800f38f36db6c3475f07f148c9562f23e
William D. Neumann <wneumann@gmail.com>
2020-12-04 09:04:07 -0600

    first cut at first star

--- Day4/Sources/day4/main.swift
+++ Day4/Sources/day4/main.swift
@@ -10,4 +10,37 @@ struct RunOptions: ParsableArguments {
 let options = RunOptions.parseOrExit()
 
 // MARK: - Actual work done here
+let input =
+  try String(contentsOf: options.inURL, encoding: .utf8)
+  .trimmingCharacters(in: .whitespacesAndNewlines)
+  .components(separatedBy: "\n\n")
 
+struct Passport {
+//  let byr: String   // (Birth Year)
+//  let cid: String?  // (Country ID)
+//  let ecl: String   // (Eye Color)
+//  let eyr: String   // (Expiration Year)
+//  let hcl: String   // (Hair Color)
+//  let hgt: String   // (Height)
+//  let iyr: String   // (Issue Year)
+//  let pid: String   // (Passport ID)
+  
+  let fields: [String: String]
+  
+  init?(_ str: String) {
+    let fieldstr: [(String, String)] =
+      str
+        .components(separatedBy: .whitespacesAndNewlines)
+        .map {
+          let arr = $0.components(separatedBy: ":")
+          return (arr[0], arr[1])
+        }
+    fields = Dictionary(uniqueKeysWithValues: fieldstr)
+    print("input: \(str)\nfieldstr: \(fieldstr)\ndict: \(fields)\n\n")
+    guard ["byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid"].compactMap({ fields[$0] }).count == 7 else { return nil }
+    
+  }
+}
+
+let validPasses = input.compactMap(Passport.init)
+print("There are \(validPasses.count) 'valid' passports.")
