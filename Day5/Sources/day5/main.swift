152638fbb142ab08ef35fbdb6fae7031509a3dac
William D. Neumann <wneumann@gmail.com>
2020-12-05 10:36:56 -0600

    Added single pass, no sort version

--- Day5/Sources/day5/main.swift
+++ Day5/Sources/day5/main.swift
@@ -12,9 +12,10 @@ let options = RunOptions.parseOrExit()
 // MARK: - Actual work done here
 let input =
   try String(contentsOf: options.inURL, encoding: .utf8)
+        .trimmingCharacters(in: .whitespacesAndNewlines)
         .components(separatedBy: .newlines)
 
-let seatID: (String) -> Int = { $0.reduce(0) { return $0 * 2 + ("FL".contains($1) ? 0 : 1) } }
+let seatID = { (pass: String) in pass.reduce(0) { $0 * 2 + ("FL".contains($1) ? 0 : 1) } }
 
 let seatIDs = input.map(seatID).sorted(by: >)
 print("The highest seat ID is \(seatIDs.first!)")
@@ -25,3 +26,17 @@ if let gap = gaps.first(where: { $0.0 - $0.1 == 2 }) {
 } else {
   print("no gap? You need to fix your code.")
 }
+
+
+// Single pass, no sort version
+let (minSeat, maxSeat, sum) =
+  input
+    .lazy
+    .map(seatID)
+    .reduce((Int.max, Int.min, 0)) { base, value in
+      if value == 0  { print("Found a zero!") }
+      return (min(base.0, value), max(base.1, value), base.2 + value)
+    }
+
+let expectedSum = (minSeat + maxSeat) * (maxSeat - minSeat + 1) / 2
+print("The highest seat ID is also \(maxSeat).\nYour seat ID is also \(expectedSum - sum)")
