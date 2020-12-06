68a485180b97f45ce8bf09c95bb0310043456c5e
William D. Neumann <wneumann@gmail.com>
2020-12-05 23:29:17 -0600

    switch to CommandLine from ArgParser

--- Day6/Sources/day6/main.swift
+++ Day6/Sources/day6/main.swift
@@ -1,32 +1,22 @@
-import ArgumentParser
-import Foundation
+    import Foundation
 
-// MARK: - Command line parsing
-struct RunOptions: ParsableArguments {
-  @Argument(help: "The location of the input file.", transform: URL.init(fileURLWithPath:))
-  var inURL: URL
-}
+    let groups =
+      try String(contentsOf: URL(fileURLWithPath: CommandLine.arguments[1]), encoding: .utf8)
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .components(separatedBy: "\n\n")
+            .map { $0.components(separatedBy: .newlines) }
 
-let options = RunOptions.parseOrExit()
+    func uniques(_ arrs: [String]) -> Int {
+      arrs.reduce(into: Set<Character>()) { base, value in
+        base.formUnion(value)
+      }.count
+    }
 
-// MARK: - Actual work done here
-let groups =
-  try String(contentsOf: options.inURL, encoding: .utf8)
-        .trimmingCharacters(in: .whitespacesAndNewlines)
-        .components(separatedBy: "\n\n")
-        .map { $0.components(separatedBy: .newlines) }
+    func repeats(_ arrs: [String]) -> Int {
+      arrs.dropFirst().reduce(into: Set<Character>(arrs.first!)) { base, value in
+        base.formIntersection(value)
+      }.count
+    }
 
-func uniques(_ arrs: [String]) -> Int {
-  arrs.reduce(into: Set<Character>()) { base, value in
-    base.formUnion(value)
-  }.count
-}
-
-func repeats(_ arrs: [String]) -> Int {
-  arrs.dropFirst().reduce(into: Set<Character>(arrs.first!)) { base, value in
-    base.formIntersection(value)
-  }.count
-}
-
-print("The total number of questions answered once by a group member: ", groups.map(uniques).reduce(0, +))
-print("The total number of questions answered by all group members: ", groups.map(repeats).reduce(0,+))
+    print("The total number of questions answered once by a group member: ", groups.map(uniques).reduce(0, +))
+    print("The total number of questions answered by all group members: ", groups.map(repeats).reduce(0,+))
