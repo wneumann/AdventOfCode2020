aa5d810f5df9af43f7f106ae51e5fc7186311b5d
William D. Neumann <wneumann@gmail.com>
2020-12-05 23:13:44 -0600

    Completed solution

--- Day6/Sources/day6/main.swift
+++ Day6/Sources/day6/main.swift
@@ -10,8 +10,23 @@ struct RunOptions: ParsableArguments {
 let options = RunOptions.parseOrExit()
 
 // MARK: - Actual work done here
-let input =
+let groups =
   try String(contentsOf: options.inURL, encoding: .utf8)
         .trimmingCharacters(in: .whitespacesAndNewlines)
-        .components(separatedBy: .newlines)
+        .components(separatedBy: "\n\n")
+        .map { $0.components(separatedBy: .newlines) }
 
+func uniques(_ arrs: [String]) -> Int {
+  arrs.reduce(into: Set<Character>()) { base, value in
+    base.formUnion(value)
+  }.count
+}
+
+func repeats(_ arrs: [String]) -> Int {
+  arrs.dropFirst().reduce(into: Set<Character>(arrs.first!)) { base, value in
+    base.formIntersection(value)
+  }.count
+}
+
+print("The total number of questions answered once by a group member: ", groups.map(uniques).reduce(0, +))
+print("The total number of questions answered by all group members: ", groups.map(repeats).reduce(0,+))
