6d6515a2de3304f91625fd5e7e3b818feb755ae8
William D. Neumann <wneumann@gmail.com>
2020-12-07 22:44:24 -0600

    Removed Arg Parser reference

--- Day7/Package.swift
+++ Day7/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "day7",
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
+//    .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -14,7 +14,7 @@ let package = Package(
     .target(
       name: "day7",
       dependencies: [
-        .product(name: "ArgumentParser", package: "swift-argument-parser")
+//        .product(name: "ArgumentParser", package: "swift-argument-parser")
       ]),
     .testTarget(
       name: "day7Tests",
