c54d5810e2ddb10382a1ce672e7784daeda3afdb
William D. Neumann <wneumann@gmail.com>
2020-12-07 22:49:48 -0600

    Day 8 bones commit

---
+++ Day8/Sources/day8/main.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let imput =
+  try String(contentsOf: URL(fileURLWithPath: CommandLine.arguments[1]), encoding: .utf8)
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: "\n")
+
