191e5791e86b273d54a499a8fbf17fc6979c9897
William D. Neumann <wneumann@gmail.com>
2020-12-09 08:15:28 -0600

    Day nine initial bones

---
+++ Day9/Sources/day9/main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let input =
+  try String(contentsOf: URL(fileURLWithPath: CommandLine.arguments[1]), encoding: .utf8)
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: "\n")
+        .compactMap(Int.init)
+
