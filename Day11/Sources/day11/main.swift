8f106003c9745ba1d4180dd516182bba38c3a4a8
William D. Neumann <wneumann@gmail.com>
2020-12-10 18:34:06 -0600

    day 11 bones

---
+++ Day11/Sources/day11/main.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// MARK: - Utilty crud
+func time<Res>(_ proc: @autoclosure () -> Res) -> (UInt64, Res) {
+  let startTime = DispatchTime.now().uptimeNanoseconds
+  let star = proc()
+  let elapsedTime = DispatchTime.now().uptimeNanoseconds - startTime
+  return (elapsedTime, star)
+}
+
+let input =
+  try String(contentsOf: URL(fileURLWithPath: CommandLine.arguments[1]), encoding: .utf8)
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: "\n")
+
+// MARK: - Real work happens here
