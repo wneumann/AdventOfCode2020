654b57c6538f98a777380b966b3784baca898cda
William D. Neumann <wneumann@gmail.com>
2020-12-12 09:31:57 -0600

    PArts 1 and 2 complete

--- Day12/Sources/day12/main.swift
+++ Day12/Sources/day12/main.swift
@@ -12,9 +12,73 @@ let input =
   try String(contentsOf: URL(fileURLWithPath: CommandLine.arguments[1]), encoding: .utf8)
         .trimmingCharacters(in: .whitespacesAndNewlines)
         .components(separatedBy: "\n")
-        .compactMap(Int.init)
 
 // MARK: - Real work happens here
+struct Ship {
+  static let forward = [0: (0, 1), 90: (1, 0), 180: (0, -1), 270: (-1, 0)]
+  var waypoint = (1, 10)
+  var heading: Int
+  var position: (Int, Int)
+  var manhattan: Int { abs(position.0) + abs(position.1) }
+  
+  init() {
+    heading = 0
+    position = (0, 0)
+  }
+  
+  mutating func move(command: String, value: Int) {
+    switch command {
+    case "N": position = (position.0 + value, position.1)
+    case "S": position = (position.0 - value, position.1)
+    case "E": position = (position.0, position.1 + value)
+    case "W": position = (position.0, position.1 - value)
+    case "R": heading = (heading + (360 - value)) % 360
+    case "L": heading = (heading + (360 + value)) % 360
+    case "F":
+      guard let (y, x) = Ship.forward[heading] else { return }
+      position = (position.0 + (y * value), position.1 + (x * value) )
+    default: return
+    }
+  }
+    
+  mutating func steer(command: String, value: Int) {
+    switch command {
+    case "N": waypoint = (waypoint.0 + value, waypoint.1)
+    case "S": waypoint = (waypoint.0 - value, waypoint.1)
+    case "E": waypoint = (waypoint.0, waypoint.1 + value)
+    case "W": waypoint = (waypoint.0, waypoint.1 - value)
+    case "R": (0..<(value / 90)).forEach { _ in waypoint = (-waypoint.1, waypoint.0) }
+    case "L": (0..<(value / 90)).forEach { _ in waypoint = (waypoint.1, -waypoint.0) }
+    case "F": position = (position.0 + (waypoint.0 * value), position.1 + (waypoint.1 * value) )
+    default: return
+    }
+  }
+}
 
-//let (elapsed2, star2) = time(totalPaths())
-//print("The total number of paths is: \(star2) | Time elapsed: \(elapsed2 / 1_000)μs")
+// MARK: - Execution and timing
+func setup(input: [String]) -> [(String, Int)] {
+  input.map {
+    (String($0.first!), Int($0.dropFirst())!)
+  }
+}
+
+let (elapsedSetup, commands) = time(setup(input: input))
+print("Setup time elapsed: \(elapsedSetup / 1_000)μs")
+
+func star1(input: [(String, Int)]) -> Int {
+  var ship = Ship()
+  for (command, value) in input { ship.move(command: command, value: value) }
+  return ship.manhattan
+}
+
+var (elapsed, star) = time(star1(input: commands))
+print("The manhattan discance is: \(star) | Time elapsed: \(elapsed / 1_000)μs")
+
+func star2(input: [(String, Int)]) -> Int {
+  var ship = Ship()
+  for (command, value) in input { ship.steer(command: command, value: value) }
+  return ship.manhattan
+}
+
+(elapsed, star) = time(star2(input: commands))
+print("The manhattan distance is: \(star) | Time elapsed: \(elapsed / 1_000)μs")
